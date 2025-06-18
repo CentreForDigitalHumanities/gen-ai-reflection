7f3a928dac3f571571eaa2ebeab88519011a8ca9
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-06-18 14:55:49 +0200

    Run Black globally

--- backend/gen_ai_reflection/common_settings.py
+++ backend/gen_ai_reflection/common_settings.py
@@ -1,37 +1,36 @@
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'livereload',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'revproxy',
-    'example'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "livereload",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "revproxy",
+    "example",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 LANGUAGES = [
-
-    ('en', 'English'),
-    ('nl', 'Nederlands'),
+    ("en", "English"),
+    ("nl", "Nederlands"),
 ]
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = "en"
 
-TIME_ZONE = 'Europe/Amsterdam'
+TIME_ZONE = "Europe/Amsterdam"
 
 USE_I18N = True
 
--- production.py
+++ production.py
@@ -4,12 +4,12 @@ from collect import *
 
 here = op.dirname(op.abspath(__file__))
 
-STATICFILES_DIRS = [op.join(here, 'static')]
+STATICFILES_DIRS = [op.join(here, "static")]
 STATIC_ROOT = None
 
 
 PROXY_FRONTEND = None  # use statically compiled files
 
 # Github Actions:
-if os.environ.get('CI'):
+if os.environ.get("CI"):
     DEBUG = True
