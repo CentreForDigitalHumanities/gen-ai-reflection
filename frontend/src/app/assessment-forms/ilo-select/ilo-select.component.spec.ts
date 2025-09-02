821a4e65089ea5bbd7ca58eb5fccce76f4c392da
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-09-02 09:09:44 +0200

    Fix failing tests

--- frontend/src/app/assessment-forms/ilo-select/ilo-select.component.spec.ts
+++ frontend/src/app/assessment-forms/ilo-select/ilo-select.component.spec.ts
@@ -25,7 +25,7 @@ describe('IloSelectComponent', () => {
         component = fixture.componentInstance;
 
         component.iloForm = mockIloForm;
-        component.formIndex = 0;
+        component.controlId = "test-control";
 
         fixture.detectChanges();
     });
