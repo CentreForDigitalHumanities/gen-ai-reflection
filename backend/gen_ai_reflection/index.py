d5d6be54815e8aedfcc9689caeba0c7f8e9d74b3
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-09-05 09:12:30 +0200

    Try iAnalyzer solution

--- backend/gen_ai_reflection/index.py
+++ backend/gen_ai_reflection/index.py
@@ -1,4 +1,4 @@
-from os import getcwd, path
+from os import path
 from django.http import HttpRequest, HttpResponse
 from django.contrib.staticfiles import finders
 from django.views.decorators.csrf import ensure_csrf_cookie
@@ -9,32 +9,9 @@ def index(request: HttpRequest):
     """Thin wrapper for the static index.html that adds the CSRF cookie."""
     language = request.LANGUAGE_CODE
     page = request.path[1:].split("/", 1)[0]
-
-    cwd = getcwd()
-
     # pre-rendered version available?
-    location = finders.find(
-        path.join(cwd, "source", "frontend", "dist", "browser", language, page, "index.html")
-    )
-
+    location = finders.find(path.join(language, page, "index.html"))
     if not location:
-        location = finders.find(
-            path.join(cwd, "source", "frontend", "dist", "browser", language, "index.html")
-        )
-
-    prerendered_location = path.join(
-        cwd, "source", "frontend", "dist", "browser", language, page, "index.html"
-    )
-    general_location = path.join(
-        cwd, "source", "frontend", "dist", "browser", language, "index.html"
-    )
-
-    print("Current working directory:", getcwd())
-    print(
-        f"Looking for prerendered location: {prerendered_location} -> Found: {bool(location)}"
-    )
-    print(
-        f"Looking for other location: {general_location} -> Found: {bool(finders.find(general_location))}"
-    )
+        location = finders.find(path.join(language, "index.html"))
 
     return HttpResponse(content=open(location))
