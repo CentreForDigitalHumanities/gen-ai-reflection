03e92257c303f663197e6d0ed69d20684db54455
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-09-17 14:55:33 +0200

    Add basic admin forms

--- backend/formdata/admin.py
+++ backend/formdata/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AssessmentForm, Adjustment, UseExample, ChallengeOpportunity
+
+
+@admin.register(Adjustment)
+class AdjustmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AssessmentForm)
+class AssessmentFormAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UseExample)
+class UseExampleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ChallengeOpportunity)
+class ChallengeOpportunityAdmin(admin.ModelAdmin):
+    pass
