23579ad3df40e916558b2c67ca18d1945124e0fb
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-09-18 11:43:24 +0200

    Remove unused attribute

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,11 +1,10 @@
-import {inject, Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
 import { ApiResponse } from "../shared/types";
-import {HttpClient, httpResource} from "@angular/common/http";
+import { httpResource } from "@angular/common/http";
 
 @Injectable({
     providedIn: "root",
 })
 export class ApiService {
-    private http = inject(HttpClient);
     serverData = httpResource<ApiResponse>(() => `/api/data/`);
 }
