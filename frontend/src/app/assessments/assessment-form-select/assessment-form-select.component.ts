66e9f0d1729a45c1408b47ed0abdd48f936d008c
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-02-13 12:13:48 +0100

    Use ngValue

--- frontend/src/app/assessments/assessment-form-select/assessment-form-select.component.ts
+++ frontend/src/app/assessments/assessment-form-select/assessment-form-select.component.ts
@@ -1,52 +1,14 @@
-import { Component, forwardRef, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { AssessmentForm } from '../../shared/types';
-import {
-    ControlValueAccessor, FormsModule,
-    NG_VALUE_ACCESSOR,
-    ReactiveFormsModule,
-} from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'gr-assessment-form-select',
-    imports: [ReactiveFormsModule, FormsModule],
+    imports: [ReactiveFormsModule],
     templateUrl: './assessment-form-select.component.html',
     styleUrl: './assessment-form-select.component.scss',
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => AssessmentFormSelectComponent),
-            multi: true,
-        },
-    ],
 })
-export class AssessmentFormSelectComponent implements ControlValueAccessor {
+export class AssessmentFormSelectComponent {
     public options = input.required<AssessmentForm[]>();
-
-    public value: number | string = "";
-
-    onChange: (value: number | string) => void = () => { };
-    onTouched: () => void = () => { };
-    disabled = false;
-
-    writeValue(value: number | string): void {
-        this.value = value === null ? "" : value;
-    }
-
-    registerOnChange(fn: (value: number | string) => void): void {
-        this.onChange = fn;
-    }
-
-    registerOnTouched(fn: () => void): void {
-        this.onTouched = fn;
-    }
-
-    setDisabledState?(isDisabled: boolean): void {
-        this.disabled = isDisabled;
-    }
-
-    onSelectionChange(selectedValue: string): void {
-        this.value = selectedValue ? parseInt(selectedValue) : "";
-        this.onChange(this.value);
-        this.onTouched();
-    }
+    public control = input.required<FormControl<number | null>>();
 }
